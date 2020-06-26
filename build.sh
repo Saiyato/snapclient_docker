0dbcaa8ef6c82786e8c1caea25649841ae0096dc
EWRGroeneveld <Saiyato@users.noreply.github.com>
2020-06-26 15:10:28 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 
 docker build \
 -t saiyato/snapclient:development \
--f DockerfileClient \
 --build-arg SNAPCASTVERSION=0.15.0 .
 
 # build for version 0.15.0, replace with necessary version, if version is suffixed with -1, add -1 as extra parameter before the final dot
